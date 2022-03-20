2217df0ce6f6f88d0daf774e67248e261eef1df3
Shawn M. Jones <jones.shawn.m@gmail.com>
2022-03-20 15:03:01 -0600

    resetting install.rdf for Zotero 6

--- raiseversion.sh
+++ raiseversion.sh
@@ -3,4 +3,6 @@
 datenow=`date '+%Y%m%d%H%M%S'`
 
 sed "s/em:version=\"2.0.0-[0-9.]*\"/em:version=\"2.0.0-$datenow\"/g" install.rdf > install.rdf-new
+sed "s/version: 2.0.0-[0-9]*/version: 2.0.0-$datenow/g" CITATION.cff > CITATION.cff-new
 mv install.rdf-new install.rdf
+mv CITATION.cff-new CITATION.cff
